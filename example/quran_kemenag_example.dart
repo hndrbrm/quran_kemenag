31a9edddf7765cbf9bffcdbe6d84ce5248a519b7
hndrbrm <hndrbrm@gmail.com>
2022-11-20 02:25:45 +0700

    Initial Files.

---
+++ example/quran_kemenag_example.dart
@@ -0,0 +1,6 @@
+import 'package:quran_kemenag/quran_kemenag.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/quran_kemenag.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library quran_kemenag;
+
+export 'src/quran_kemenag_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
